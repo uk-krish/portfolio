4aaf5532cf3cf8b6c99b2cb6321b7260304d98c7
Unnikrishnan.c <66357763+uk-krish@users.noreply.github.com>
2023-09-18 21:51:34 +0530

    Update App.js

--- Portfolio/src/App.js
+++ Portfolio/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './index.css'
 import Home from "./components/Home";
 import About from "./components/About";
@@ -7,18 +7,32 @@ import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 function App() {
+  const [loading, setLoading] = useState(true);
   window.addEventListener('contextmenu', function (e) {
     e.preventDefault();
   }, false);
+ useEffect(()=>{
+  const spinner = document.getElementById("spinner");
+  if (spinner) {
+    setTimeout(() => {
+      spinner.style.display = "none";
+      setLoading(false);
+    }, 2000);
+  }
+ },[loading])
   return (
     
-    <div >
-      <Navbar />
-      <Home/>
-      <About/>
-      <Projects/>
-      <Contact/>
-      <Footer/>
+    <div>
+      {!loading && (
+        <>
+          <Navbar />
+          <Home />
+          <About />
+          <Projects />
+          <Contact />
+          <Footer />
+        </>
+      )}
     </div>
   );
 }
